bf74cddfe760f436aa917749380bbc7fd5134418
Aiden Lignieres <46545209+aidenlignieres@users.noreply.github.com>
2023-03-08 13:49:29 +1000

    Task 2 & 3 Completed

--- app/src/main/java/com/example/quicksumapp/MainActivity.java
+++ app/src/main/java/com/example/quicksumapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.quicksumapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public <View> void buttonClicked(View view) {
+        Button button = (Button) view;
+
+        int sum = Integer.parseInt(button.getText().toString());
+
+        TextView textView = findViewById(R.id.sum);
+        String result = "" + sum;
+        textView.setText(result);
     }
 }
